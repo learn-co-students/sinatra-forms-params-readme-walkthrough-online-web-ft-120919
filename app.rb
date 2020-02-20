146c745c1454cf8a2c205654b4b27309a688a85f
Nat McConnaughay <natmcconnaughay@gmail.com>
2020-02-19 19:16:13 -0500

    Done.

--- app.rb
+++ app.rb
@@ -6,6 +6,12 @@ class App < Sinatra::Base
     erb :food_form
   end
 
+  post '/food' do
+    @name = params[:name]
+    @favorite_food = params[:favorite_food]
+    "My name is #{@name}, and I love #{@favorite_food}"
+  end
+
   # Add your post route and action below
 
 end
